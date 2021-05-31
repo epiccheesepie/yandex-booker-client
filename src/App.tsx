6e4bf7b0732f45dee7fdc56b6da6da5cdc286a3f
epiccheesepie <boo-baa-boo@yandex.ru>
2021-05-31 21:43:23 +0300

    add search request

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
 import * as React from 'react';
 import { Bar } from './components';
 
+const mapDocsToSnippet = (docs) => docs.filter(book => !!book.author_name && !!book.cover_i).map(book => {
+    return {
+        title: book.title,
+        author: book.author_name[0],
+        img: `http://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+    };
+});
+
 const App = () => {
 
     const timer : any = React.useRef();
     const [query, setQuery] = React.useState('');
-    const handleChange = (e) => {
-        clearTimeout(timer.current);
-        const text : string = e.target.value;
-        setQuery(text);
+    const handleChange = (e) => setQuery(e.target.value);
 
-        if (text.length > 0) {
+    const [books, setBooks] = React.useState([]);
+    
+    React.useEffect(() => {
+        if (query.length > 0) {
             timer.current = setTimeout(() => {
-                //делаем запрос в апи
+                fetch('http://openlibrary.org/search.json?' + new URLSearchParams({
+                    title: query
+                }))
+                .then(res => {
+                    res.json()
+                    .then(res => {
+                        const books = mapDocsToSnippet(res.docs);
+                        setBooks(books);
+                        console.log(books);
+                        console.log(res.docs);
+                    });
+                });
             }, 1000);
         }
-    };
+
+        return () => {
+            clearTimeout(timer.current);
+        }
+    }, [query]);
 
     return (
         <main>
